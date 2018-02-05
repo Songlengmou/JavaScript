b71af6c7ddd3faa8ea5384a4300f6818e0f93d8b
hyf_syp <hyf@syp.com>
2018-02-05 16:17:01 +0800

    引入外部js文件

--- L02_JS_Introduce/app.js
+++ L02_JS_Introduce/app.js
@@ -10,10 +10,10 @@
 //     $("#container").html("Hello World");
 // })
 
-//3. 这个方法要求要在HTML文件中的body标签里 添加一个onload="bodyLoaded()"监听  (eg:<body onload="bodyLoaded()">)
+//3. 这个方法要求要在html文件中的body标签里 添加一个onload="bodyLoaded()"监听  (eg:<body onload="bodyLoaded()">)
 // function bodyLoaded() {
 //     $("#container").html("Hello World");
 // }
 
-//4.要求 HTML文件的 app.js要放到body标签的最后一行
+//4.要求 html文件的 app.js要放到body标签的最后一行
 $("#container").html("Hello World");
