90d0c103d62a35d5fe84373286e353f0539f5167
hyf_syp <hyf@syp.com>
2018-02-06 16:57:47 +0800

    变量与常量

---
+++ L03_JS_Amount/app.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 20;
+a = 100;
+var c = a + b;
+
+console.log(a, b, c);
+
+//ES5    ES6
+
+var MATH_PI = 3.14;
+console.log(MATH_PI);
+
+
+// const MATH_PI = 3.14; //ES6支持，ES5不行
