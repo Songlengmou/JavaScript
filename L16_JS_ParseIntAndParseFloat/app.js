2265629bbb2cd6428462faede6517ddadc918ee0
hyf_syp <hyf@syp.com>
2018-03-07 11:21:06 +0800

    parseInt和parseFloat

---
+++ L16_JS_ParseIntAndParseFloat/app.js
@@ -0,0 +1,12 @@
+(function () {
+
+    // console.log(parseInt("100" ,2)); //二进制
+    // console.log(parseInt("A" ,16)); //十六进制
+
+    // console.log("3.14" + 2);
+    // console.log(parseFloat("3.14") + 2);//一般不用浮点类型,因为误差
+
+    //数字转成字符串
+    var num = 200;
+    console.log("" + num + 2);
+})();
