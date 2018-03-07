40c51588e8b33a7c2c8f36a4c4c55d218a44d82c
hyf_syp <hyf@syp.com>
2018-03-07 16:49:54 +0800

    async和await (异步编程)

---
+++ L26_JS_asyncAndawait/app.js
@@ -0,0 +1,42 @@
+(function () {
+
+    //异步编程
+    // function loadData(url) {
+    //     return new Promise(function (resovle, reject) {
+    //         let xhr = new XMLHttpRequest();
+    //         xhr.onload = function () {
+    //             resovle(xhr.responseText);
+    //         };
+    //         xhr.open("get", url);
+    //         xhr.send();
+    //     });
+    // }
+    //
+    // async function init() {
+    //     let result = await loadData("data1.txt");
+    //     console.log(result);
+    // }
+    //
+    // init();
+
+    //-------------------------------------
+    function loadData(url) {
+        return new Promise(function (resovle, reject) {
+            let xhr = new XMLHttpRequest();
+            xhr.onload = function () {
+                resovle(xhr.responseText);
+            };
+            xhr.open("get", url);
+            xhr.send();
+        });
+    }
+
+    async function init() {
+        let urls = ["data1.txt", "data2.txt", "data3.txt"];
+        for (let url of urls) {
+            console.log(await loadData(url));
+        }
+    }
+
+    init();
+})();
