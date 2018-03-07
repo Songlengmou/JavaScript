866646ea70852bda7efbc0aad9529222938d49e9
hyf_syp <hyf@syp.com>
2018-03-07 11:32:05 +0800

    isNaN和isFinite

---
+++ L17_JS_isNaNAndisFinite/app.js
@@ -0,0 +1,13 @@
+(function () {
+
+    // var value = 5 - "Hello";
+    // console.log(isNaN(value));
+
+    var value = 5 - "Hello";
+    value = 1 / 0;
+    // console.log(value==Infinity);
+    // console.log(isFinite(value));
+    // console.log(isFinite(100));
+    console.log(100 === Infinity);
+
+})();
